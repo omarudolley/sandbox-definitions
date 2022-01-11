277ba104c6befdea1fb7ee1a2ffb92a78a2d5477
Joakim Nordling <joakim.nordling@digitalliving.fi>
2022-01-11 09:44:42 +0200

    Add definition for Country/BasicInfo (#3)

---
+++ src/test/ioxio-nexus-guides/Country/BasicInfo.py
@@ -0,0 +1,85 @@
+from typing import List, Optional
+
+from converter import CamelCaseModel, DataProductDefinition
+from pydantic import Field
+
+
+class BasicCountryInfoRequest(CamelCaseModel):
+    code: str = Field(
+        ...,
+        title="Code",
+        description="ISO 3166-1 alpha-2 code for the country",
+        example="FI",
+        min_length=2,
+        max_length=2,
+    )
+
+
+class Capital(CamelCaseModel):
+    name: str = Field(
+        ...,
+        title="Name",
+        description="The name of the capital of the Country",
+        example="Helsinki",
+    )
+    lat: float = Field(
+        ...,
+        title="Latitude",
+        description="The latitude coordinate of the Capital",
+        ge=-90.0,
+        le=90.0,
+        example=60.170833,
+    )
+    lon: float = Field(
+        ...,
+        title="Longitude",
+        description="The longitude coordinate of the Capital",
+        ge=-180.0,
+        le=180.0,
+        example=24.9375,
+    )
+
+
+class BasicCountryInfoResponse(CamelCaseModel):
+    code: str = Field(
+        ...,
+        title="Code",
+        description="ISO 3166-1 alpha-2 code for the country",
+        example="FI",
+        min_length=2,
+        max_length=2,
+    )
+    name: str = Field(
+        ...,
+        title="Name",
+        description="The name of the country",
+        example="Finland",
+    )
+    area: float = Field(
+        ...,
+        title="Area",
+        description="The area of the country in km^2",
+        example=338455,
+    )
+    languages: List[str] = Field(
+        ...,
+        title="Official languages",
+        description="ISO 639-1 language codes for the official languages",
+        example=["fi", "sv"],
+        min_length=2,
+        max_length=2,
+    )
+    capital: Optional[Capital] = Field(
+        None,
+        title="Capital",
+        description="The capital of the country, legislative if multiple",
+    )
+
+
+DEFINITION = DataProductDefinition(
+    description="Data Product for basic country info",
+    request=BasicCountryInfoRequest,
+    response=BasicCountryInfoResponse,
+    route_description="Information about the country",
+    summary="Basic Country Info",
+)
