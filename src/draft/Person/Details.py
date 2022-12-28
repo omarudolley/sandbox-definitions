03af650aec2e7d31deb717b09b8e162adbdc3352
fbjorn <fbjorn@users.noreply.github.com>
2022-12-28 16:52:32 +0300

    Add draft/Person/Details definition

---
+++ src/draft/Person/Details.py
@@ -0,0 +1,29 @@
+from converter import CamelCaseModel, DataProductDefinition
+from pydantic import Field
+
+
+class PersonDetailsRequest(CamelCaseModel):
+    pass
+
+
+class PersonDetailsResponse(CamelCaseModel):
+    name: str = Field(
+        ...,
+        title="Full name",
+        description="Person's name and surname",
+        example="Joshua Gray",
+    )
+    address: str = Field(
+        ...,
+        title="Address",
+        description="Person's home address",
+        example="6 Raymond river\nRileybury\nCR3 6XA",
+    )
+
+
+DEFINITION = DataProductDefinition(
+    request=PersonDetailsRequest,
+    response=PersonDetailsResponse,
+    summary="Person details",
+    requires_authorization=True,
+)
